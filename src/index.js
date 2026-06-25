e67e1203fe8f97e0f4353a50a7b492bf0b8ae4ff
marmooo <marmooo@users.noreply.github.com>
2026-06-26 07:38:39 +0900

    bump @marmooo/midy from 0.5.6 to 0.5.7

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { Midy } from "https://cdn.jsdelivr.net/gh/marmooo/midy@0.5.6/dist/midy.min.js";
+import { Midy } from "https://cdn.jsdelivr.net/gh/marmooo/midy@0.5.7/dist/midy.min.js";
 
 function toggleDarkMode() {
   const html = document.documentElement;
--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const cacheName = "2026-06-16 00:00";
+const cacheName = "2026-06-26 00:00";
 const urlsToCache = [
   "/isotone/index.js",
   "/isotone/favicon/favicon.svg",
